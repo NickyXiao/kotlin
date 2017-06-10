e0279eb9b8e01b3c9173077cf5733aa9736f3e50
shiraji <isogai.shiraji@gmail.com>
2017-06-11 01:50:45 +0300

    Add better quickfix for scope functions #KT-13676 Fixed

--- idea/src/org/jetbrains/kotlin/idea/quickfix/ReplaceCallFix.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/ReplaceCallFix.kt
@@ -26,6 +26,8 @@ import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
 import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCall
 import org.jetbrains.kotlin.resolve.calls.resolvedCallUtil.getImplicitReceiverValue
+import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameUnsafe
 
 abstract class ReplaceCallFix(
         expression: KtQualifiedExpression,
@@ -79,6 +81,52 @@ class ReplaceWithSafeCallFix(expression: KtDotQualifiedExpression): ReplaceCallF
             }
         }
     }
+}
+
+class ReplaceWithSafeCallForScopeFunctionFix(expression: KtDotQualifiedExpression) : ReplaceCallFix(expression, "?.") {
+
+    override fun getText() = "Replace scope function with safe (?.) call"
+
+    companion object : KotlinSingleIntentionActionFactory() {
+        override fun createAction(diagnostic: Diagnostic): KotlinQuickFixAction<KtExpression>? {
+            val element = diagnostic.psiElement
+            val scopeFunctionLiteral = element.getStrictParentOfType<KtFunctionLiteral>() ?: return null
+            val scopeCallExpression = scopeFunctionLiteral.getStrictParentOfType<KtCallExpression>() ?: return null
+            val scopeDotQualifiedExpression = scopeCallExpression.getStrictParentOfType<KtDotQualifiedExpression>() ?: return null
+            val scopeFunctionKind = scopeCallExpression.scopeFunctionKind() ?: return null
+
+            val internalReceiverExpressionText = (element.parent as? KtDotQualifiedExpression)?.receiverExpression?.text
+
+            when (scopeFunctionKind) {
+                ScopeFunctionKind.WITH_RECEIVER -> {
+                    if (scopeFunctionLiteral.hasParameterSpecification()) {
+                        if (internalReceiverExpressionText == null ||
+                            internalReceiverExpressionText != scopeFunctionLiteral.valueParameters.singleOrNull()?.text) return null
+                    }
+                    else {
+                        if (internalReceiverExpressionText != "it") return null
+                    }
+                }
+                ScopeFunctionKind.WITH_THIS -> {
+                    if (internalReceiverExpressionText != "this" && internalReceiverExpressionText != null) return null
+                }
+            }
+
+            return ReplaceWithSafeCallForScopeFunctionFix(scopeDotQualifiedExpression)
+        }
+
+        private fun KtCallExpression.scopeFunctionKind(): ScopeFunctionKind? {
+            val resolvedCall = this.getResolvedCall(this.analyze()) ?: return null
+            val methodName = resolvedCall.resultingDescriptor.fqNameUnsafe.asString()
+            return ScopeFunctionKind.values().firstOrNull { kind -> kind.names.contains(methodName) }
+        }
+
+        private enum class ScopeFunctionKind(vararg val names: String) {
+            WITH_RECEIVER("kotlin.let", "kotlin.also"),
+            WITH_THIS("kotlin.apply", "kotlin.run")
+        }
+    }
+
 }
 
 class ReplaceWithDotCallFix(expression: KtSafeQualifiedExpression): ReplaceCallFix(expression, "."), CleanupFix {
